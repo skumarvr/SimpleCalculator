80d1287891ed41918f22c5b36aa75fa21521028f
Sajith <sajith@metarouge.com.au>
2021-10-24 12:51:27 +1100

    Implemented simple console application
    7.	Implement a simple console application using Visual Studio that will use the class.

---
+++ CalculatorTest.App/Program.cs
@@ -0,0 +1,23 @@
+﻿using CalculatorTest.Lib;
+using System;
+
+namespace CalculatorTest.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simpe Calculator!");
+
+            IDiagnostics diag = new ConsoleDiagnostics();
+            ISimpleCalculator simpleCalc = new SimpleCalculator(diag);
+
+            simpleCalc.Add(10, 10);
+            simpleCalc.Subtract(10, 10);
+            simpleCalc.Multiply(10, 10);
+            simpleCalc.Divide(10, 10);
+
+            var waitForInput = Console.ReadLine();
+        }
+    }
+}
